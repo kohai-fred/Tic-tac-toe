cf31116432219d16740f6f4e1c95c68066f28bce
Fred <frederick.vu.pro@gmail.com>
2020-11-25 20:58:50 +0100

    add IA player! and cleaning code

--- src/index.js
+++ src/index.js
@@ -15,154 +15,216 @@ let nbParts = 0;
 let nbWinsP1 = 0;
 let nbWinsP2 = 0;
 let nullMatch = 0;
+let ordiRand;
+let targetOrdi;
 
 const activeToggle = () => {
-	if (player1.classList.contains("active")) {
-		player1.classList.remove("active");
-	} else {
-		player1.classList.add("active");
-	}
+  player1.classList.contains("active")
+    ? player1.classList.remove("active")
+    : player1.classList.add("active");
 
-	if (player2.classList.contains("active")) {
-		player2.classList.remove("active");
-	} else {
-		player2.classList.add("active");
-	}
+  player2.classList.contains("active")
+    ? player2.classList.remove("active")
+    : player2.classList.add("active");
 };
 
 const createCross = (target) => {
-	const cross = document.createElement("i");
-	cross.classList.add("fas", "fa-times");
-	target.insertAdjacentElement("afterbegin", cross);
+  const cross = document.createElement("i");
+  cross.classList.add("fas", "fa-times");
+  target.insertAdjacentElement("afterbegin", cross);
 };
-const createCircle = (target) => {
-	const circle = document.createElement("i");
-	circle.classList.add("far", "fa-circle");
-	target.insertAdjacentElement("afterbegin", circle);
+const createCircle = (target, targetOrdi) => {
+  const circle = document.createElement("i");
+  circle.classList.add("far", "fa-circle");
+  target
+    ? target.insertAdjacentElement("afterbegin", circle)
+    : targetOrdi.insertAdjacentElement("afterbegin", circle);
 };
-
 const createModal = (content) => {
-	modal = document.createElement("div");
-	modal.classList.add("modal");
-	modal.append(content);
-	gameContent.insertAdjacentElement("afterbegin", modal);
+  modal = document.createElement("div");
+  modal.classList.add("modal");
+  modal.append(content);
+  gameContent.insertAdjacentElement("afterbegin", modal);
 };
 
 const displayCounters = () => {
-	playersContent.children[1].textContent = nbParts;
-	playersContent.firstElementChild.lastElementChild.textContent = nbWinsP1;
-	playersContent.lastElementChild.lastElementChild.textContent = nbWinsP2;
+  playersContent.children[1].textContent = nbParts;
+  playersContent.firstElementChild.lastElementChild.textContent = nbWinsP1;
+  playersContent.lastElementChild.lastElementChild.textContent = nbWinsP2;
+};
+const randomNum = () => {
+  ordiRand = Math.floor(Math.random() * 9);
+  targetOrdi = gameContent.children[ordiRand];
 };
 
-const check = (a, b, c) => {
-	if (
-		arraySquareId[a] == arraySquareId[b] &&
-		arraySquareId[b] == arraySquareId[c]
-	) {
-		createModal(arraySquareId[a] + " WIN !!!");
-		if (arraySquareId[a] == player1.firstElementChild.textContent) {
-			nbWinsP1++;
-			nbParts++;
-			displayCounters();
-			console.log(nbWinsP1);
-		}
-		if (arraySquareId[a] == player2.firstElementChild.textContent) {
-			nbWinsP2++;
-			nbParts++;
-			displayCounters();
-			console.log(arraySquareId[a]);
-		}
-	}
+// check the possibilities of winning
+const checkToWin = (a, b, c) => {
+  if (
+    arraySquareId[a] == arraySquareId[b] &&
+    arraySquareId[b] == arraySquareId[c]
+  ) {
+    if (!gameContent.firstElementChild.classList.contains("modal")) {
+      createModal(arraySquareId[a] + " WIN !!!");
+    }
+    if (arraySquareId[a] == player1.firstElementChild.textContent) {
+      nbWinsP1++;
+      nbParts++;
+      displayCounters();
+    }
+    if (arraySquareId[a] == player2.firstElementChild.textContent) {
+      nbWinsP2++;
+      nbParts++;
+      displayCounters();
+    }
+  }
+  // if nobody win...
+  if (!gameContent.firstElementChild.classList.contains("modal")) {
+    if (nullMatch == 9) {
+      createModal("Match null...");
+      nbParts++;
+      displayCounters();
+    }
+  }
+};
+const checkPossibility = () => {
+  nullMatch++;
+  checkToWin(0, 1, 2);
+  checkToWin(3, 4, 5);
+  checkToWin(6, 7, 8);
+  checkToWin(0, 3, 6);
+  checkToWin(1, 4, 7);
+  checkToWin(2, 5, 8);
+  checkToWin(0, 4, 8);
+  checkToWin(2, 4, 6);
+};
+
+const iaTurn = (target) => {
+  while (
+    !targetOrdi.classList.contains("cursor") ||
+    target.id == targetOrdi.id
+  ) {
+    randomNum();
+
+    if (
+      targetOrdi.classList.contains("cursor") &&
+      target.id !== targetOrdi.id
+    ) {
+      arraySquareId[ordiRand] = player2.firstElementChild.textContent;
+      break;
+    }
+  }
+  insertArraySquare(target, targetOrdi);
+  createCircle(targetOrdi);
+  activeToggle();
+  targetOrdi.classList.remove("cursor");
+  checkPossibility();
+};
+const insertArraySquare = (target, targetOrdi) => {
+  for (let i = 0; i < arraySquareId.length; i++) {
+    if (selectPlayer.value !== selectPlayer[0].value) {
+      if (arraySquareId[i] == target.id) {
+        if (player1.classList.contains("active")) {
+          arraySquareId[i] = player1.firstElementChild.textContent;
+        }
+        if (player2.classList.contains("active")) {
+          arraySquareId[i] = player2.firstElementChild.textContent;
+        }
+      }
+    }
+
+    if (selectPlayer.value == selectPlayer[0].value) {
+      if (arraySquareId[i] == target.id) {
+        arraySquareId[i] = player1.firstElementChild.textContent;
+      }
+      if (arraySquareId[i] == targetOrdi.id) {
+        arraySquareId[i] = player2.firstElementChild.textContent;
+      }
+    }
+  }
 };
 
 squares.forEach((square) => {
-	arraySquareId.push(square.id);
+  arraySquareId.push(square.id);
 
-	square.classList.add("cursor");
-	square.addEventListener("click", (event) => {
-		let target = event.target;
+  square.classList.add("cursor");
+  square.addEventListener("click", (event) => {
+    let target = event.target;
 
-		if (target.classList.contains("cursor")) {
-			if (player1.classList.contains("active")) {
-				createCross(target);
-			}
-			if (player2.classList.contains("active")) {
-				createCircle(target);
-			}
-		}
+    if (target.classList.contains("cursor")) {
+      if (player1.classList.contains("active")) {
+        insertArraySquare(target, square);
+        createCross(target);
+        checkPossibility();
+      }
 
-		for (let i = 0; i < arraySquareId.length; i++) {
-			if (arraySquareId[i] == target.id) {
-				if (player1.classList.contains("active")) {
-					arraySquareId[i] = player1.firstElementChild.textContent;
-				}
-				if (player2.classList.contains("active")) {
-					arraySquareId[i] = player2.firstElementChild.textContent;
-				}
-			}
-		}
+      // For IA
+      if (selectPlayer.value == selectPlayer[0].value) {
+        randomNum();
+      }
 
-		check(0, 1, 2);
-		check(3, 4, 5);
-		check(6, 7, 8);
-		check(0, 3, 6);
-		check(1, 4, 7);
-		check(2, 5, 8);
-		check(0, 4, 8);
-		check(2, 4, 6);
+      // For 2eme human player
+      if (player2.classList.contains("active")) {
+        insertArraySquare(target, square);
+        createCircle(target);
+        checkPossibility();
+      }
 
-		nullMatch++;
-		if (nullMatch == 9) {
-			createModal("Match null...");
-			nbParts++;
-			displayCounters();
-		}
+      // launch the AI ​​tour
+      if (
+        selectPlayer.value == selectPlayer[0].value &&
+        !gameContent.firstElementChild.classList.contains("modal")
+      ) {
+        setTimeout(function () {
+          iaTurn(target, targetOrdi);
+        }, 300);
+      }
+    }
 
-		if (square.classList.contains("cursor")) {
-			activeToggle();
-		}
-		target.classList.remove("cursor");
-	});
+    // Toggle active player
+    if (square.classList.contains("cursor")) {
+      activeToggle();
+    }
+    target.classList.remove("cursor");
+  });
 
-	//For reload game
-	const reload = () => {
-		if (!square.hasChildNodes()) {
-			const toTrash = document.createElement("span");
-			square.insertAdjacentElement("afterbegin", toTrash);
-		}
-		if (!player1.classList.contains("active")) {
-			activeToggle();
-		}
-		if (gameContent.firstElementChild.classList.contains("modal")) {
-			modal.remove();
-		}
+  //For reload game
+  const reload = () => {
+    if (!square.hasChildNodes()) {
+      const toTrash = document.createElement("span");
+      square.insertAdjacentElement("afterbegin", toTrash);
+    }
+    if (!player1.classList.contains("active")) {
+      activeToggle();
+    }
+    if (gameContent.firstElementChild.classList.contains("modal")) {
+      modal.remove();
+    }
 
-		square.firstChild.remove();
-		square.classList.add("cursor");
-		// modal.remove();
-		nullMatch = 0;
+    square.firstChild.remove();
+    square.classList.add("cursor");
+    nullMatch = 0;
 
-		arraySquareId = [];
-		squares.forEach((square) => {
-			arraySquareId.push(square.id);
-		});
-	};
-	btnReload.addEventListener("click", reload);
+    arraySquareId = [];
+    squares.forEach((square) => {
+      arraySquareId.push(square.id);
+    });
+  };
+  btnReload.addEventListener("click", reload);
 
-	content.addEventListener("click", (event) => {
-		event.stopPropagation();
-		if (gameContent.firstElementChild.classList.contains("modal")) {
-			modal.addEventListener("click", reload);
-		}
-	});
+  content.addEventListener("click", (event) => {
+    event.stopPropagation();
+    if (gameContent.firstElementChild.classList.contains("modal")) {
+      modal.addEventListener("click", reload);
+    }
+  });
 
-	// For change opponent
-	selectPlayer.addEventListener("change", () => {
-		player2.firstElementChild.textContent = selectPlayer.value;
-		reload();
-		nbParts = 0;
-		nbWinsP1 = 0;
-		nbWinsP2 = 0;
-		displayCounters();
-	});
+  // For change opponent
+  selectPlayer.addEventListener("change", () => {
+    player2.firstElementChild.textContent = selectPlayer.value;
+    reload();
+    nbParts = 0;
+    nbWinsP1 = 0;
+    nbWinsP2 = 0;
+    displayCounters();
+  });
 });
